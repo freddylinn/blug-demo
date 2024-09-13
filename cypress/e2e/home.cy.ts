1a51082166652f1e1715642da9b72cd57d804db3
Freddy <frederick.linn@colorado.edu>
2024-09-12 19:33:55 -0600

    Added new cypress tests and github workflow

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,5 +3,11 @@ describe("The Home Page", () => {
     cy.visit("/");
   });
 
+  it("contains a button to /hobbies", () => {
+    cy.visit("/");
+    cy.get("button").should("exist");
+    cy.get("button").click();
+    cy.url().should("include", "/hobbies");
+  });
   // Add the test for the link to the Hobbies page here
 });
